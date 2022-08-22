ad15451841d54a5d4192abe78dc264f06eb352a0
Olarewaju Temtope Albert <tmtpalbert@gmail.com>
2022-08-22 14:25:08 +0100

    Initial commit

---
+++ Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject plane;
+    private Vector3 offset = new Vector3(11.8f,2.26f,0.0f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = plane.transform.position + offset;
+    }
+}
